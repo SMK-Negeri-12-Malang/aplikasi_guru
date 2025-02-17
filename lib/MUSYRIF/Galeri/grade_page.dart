6abc216e70cad0e3aad707d318815fafd82f59dd
scchanz <izzuncz@gmail.com>
2025-02-17 09:48:07 +0700

    pergantian grade menjadi galeri

---
+++ lib/MUSYRIF/Galeri/grade_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GaleriPage
+ extends StatefulWidget {
+  const GaleriPage
+  ({super.key});
+
+  @override
+  State<GaleriPage
+  > createState() => _GradePageState();
+}
+
+class _GradePageState extends State<GaleriPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
