e790f9ce11ad4bd00cefb172583bbbf722def91c
scchanz <izzuncz@gmail.com>
2025-03-18 14:17:19 +0700

    pembaruan tugas santri musryf

---
+++ lib/models/tugassantri_model.dart
@@ -0,0 +1,59 @@
+class SesiData {
+  static List<Map<String, dynamic>> sesiList = [
+    {
+      "sesi": "Siang",
+      "kategori": [
+        {
+          "nama": "Tahsin",
+          "kegiatan": ["Belajar Tajwid", "Latihan Makhraj", "Praktik Bacaan"]
+        },
+        {
+          "nama": "Tahfidz",
+          "kegiatan": ["Setoran Hafalan", "Muroja'ah", "Latihan Hafalan"]
+        },
+        {
+          "nama": "Mutabaah",
+          "kegiatan": ["Shalat Dzuhur", "Makan Siang", "Kegiatan Siang"]
+        }
+      ]
+    },
+    {
+      "sesi": "Sore",
+      "kategori": [
+        {
+          "nama": "Tahsin",
+          "kegiatan": [
+            "Latihan Tajwid",
+            "Praktik Makhraj",
+            "Latihan Bacaan Quran"
+          ]
+        },
+        {
+          "nama": "Tahfidz",
+          "kegiatan": ["Setoran Hafalan", "Muroja'ah", "Latihan Hafalan"]
+        },
+        {
+          "nama": "Mutabaah",
+          "kegiatan": ["Shalat Ashar", "Kegiatan Sore"]
+        }
+      ]
+    },
+    {
+      "sesi": "Malam",
+      "kategori": [
+        {
+          "nama": "Tahsin",
+          "kegiatan": ["Muroja'ah", "Praktik Tajwid", "Latihan Hafalan"]
+        },
+        {
+          "nama": "Tahfidz",
+          "kegiatan": ["Setoran Hafalan", "Muroja'ah"]
+        },
+        {
+          "nama": "Mutabaah",
+          "kegiatan": ["Shalat Isya", "Kegiatan Malam"]
+        }
+      ]
+    }
+  ];
+}
