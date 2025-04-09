6eecfa86c19a6366766586bdfbf401f8de081933
scchanz <izzuncz@gmail.com>
2025-04-09 10:27:36 +0700

    design kepesantrenan

--- lib/GURU_QURAN/Kepesantrenan/Kepesantrenan.dart
+++ lib/GURU_QURAN/Kepesantrenan/Kepesantrenan.dart
@@ -385,7 +385,7 @@ class _KepesantrenanState extends State<Kepesantrenan> {
             ),
             const SizedBox(height: 5),
             Text(
-              'Program Tahfidz & Tahsin',
+              'Program Tahfidz & Tahsin',//tes
               style: TextStyle(
                 fontSize: 14,
                 color: Colors.white.withOpacity(0.9),
